83750a72bc63309fcd93543af2965644eaeb3037
Atreyee Mondal (atrmonda) <atrmonda@cisco.com>
2021-10-01 10:57:28 +0530

    Changes Saved

---
+++ src/com/sort/SelectionSortDemo.java
@@ -0,0 +1,35 @@
+package com.sort;
+public class SelectionSortDemo {
+
+    public static void main(String[] args) {
+
+    int[] arr = {9,6,3,1,2,4,5};
+    int length = arr.length;
+    SelectionSortDemo(arr);
+    System.out.println("The sorted elements are:");
+    for(int i:arr){
+
+        System.out.println(i);
+         }
+     }
+
+    public static void SelectionSortDemo(int[] arr){
+
+        for(int i=0;i<arr.length-1;i++){
+
+            int index =i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[index]){
+
+                    index =j;
+                }
+
+            }
+            int smallNumber = arr[index];
+            arr[index]= arr[i];
+            arr[i]= smallNumber;
+        }
+
+    }
+}
+
